8478739544cb2f22bde2b903efec30e48877591e
evangelos-ch <angelos.h@hotmail.com>
2022-02-14 15:47:28 +0000

    #3: Auto scroll & fades

--- assets/script/project-showcase.js
+++ assets/script/project-showcase.js
@@ -3,21 +3,56 @@ const projectShowcase = document.getElementById("project-showcase");
 if (projectShowcase) {
     const activeItem = document.getElementById("active-item");
     const allItems = document.getElementById("all-items");
-
     let currentActiveItem = null;
-    const setCurrentActiveItem = (newActiveItem) => {
-        newActiveItem.classList.add("all-items__active");
-        if (currentActiveItem) {
-            currentActiveItem.classList.remove("all-items__active");
-            activeItem.children[0].replaceWith(newActiveItem.cloneNode((deep = true)));
-        } else {
-            activeItem.appendChild(newActiveItem.cloneNode((deep = true)));
+    let timerCancelled = false;
+
+    // Poll to check if the user is watching a video every second and cancel the timer
+    setInterval(() => {
+        if (document.activeElement && document.activeElement.tagName == "IFRAME") {
+            timerCancelled = true;
         }
-        currentActiveItem = newActiveItem;
+    }, 500);
+
+    const setCurrentActiveItem = (i) => {
+        if (currentActiveItem != null) {
+            activeItem.children[0].style.opacity = "0";
+            activeItem.children[0].addEventListener("transitionend", () => {
+                // Get rid of old element
+                activeItem.children[0].remove();
+                allItems.children[currentActiveItem].classList.remove("all-items__active");
+
+                // Add new element at 0 opacity
+                const child = allItems.children[i].cloneNode((deep = true));
+                child.style.opacity = "0";
+                activeItem.appendChild(child);
+                allItems.children[i].classList.add("all-items__active");
+                currentActiveItem = i;
+
+                // Set its opacity to 1 a tick later:
+                setTimeout(() => (activeItem.children[0].style.opacity = "1"), 100);
+            });
+        } else {
+            const child = allItems.children[i].cloneNode((deep = true));
+            activeItem.appendChild(child);
+            currentActiveItem = i;
+        }
+    };
+
+    const scrollToNextItem = () => {
+        if (!timerCancelled) setCurrentActiveItem((currentActiveItem + 1) % allItems.children.length);
+    };
+    const createCarouselTimer = () => {
+        timerCancelled = false;
+        return setInterval(scrollToNextItem, 10_000);
     };
 
     // Initialize the active item
-    setCurrentActiveItem(allItems.children[0]);
+    setCurrentActiveItem(0);
+    let carouselTimer = createCarouselTimer();
+    const resetTimer = () => {
+        clearInterval(carouselTimer);
+        carouselTimer = createCarouselTimer();
+    };
 
     // Handle mouse clicks
     for (let i = 0; i < allItems.children.length; ++i) {
@@ -26,7 +61,8 @@ if (projectShowcase) {
         child.addEventListener("click", (e) => {
             e.stopPropagation();
             e.preventDefault();
-            setCurrentActiveItem(allItems.children[i]);
+            setCurrentActiveItem(i);
+            resetTimer();
         });
     }
 }
